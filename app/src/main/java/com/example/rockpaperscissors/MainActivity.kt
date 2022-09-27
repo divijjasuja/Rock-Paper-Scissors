07660e7b6d99343b2f241e5ce79d0f224749fde3
divij jasuja <divijjasuja@gmail.com>
2022-09-27 09:27:02 +0530

    added Choice, jetpack text, ButtonBox(not complete)

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.rockpaperscissors
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+//import androidx.compose.foundation.layout.ColumnScopeInstance.weight
+//import androidx.compose.foundation.layout.RowScopeInstance.weight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,9 +63,58 @@ fun Score() {
 }
 
 @Composable
+fun Choice(player:String,android:String) {
+    Row(modifier = Modifier
+        .width(16.dp)
+        .wrapContentWidth(Alignment.CenterHorizontally)
+        .wrapContentHeight(Alignment.CenterVertically),
+        horizontalArrangement = Arrangement.spacedBy(100.dp)
+    ) {
+        Column() {
+            Text(text = "your choice",fontSize = 16.sp)
+            Text(text = "$player",fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        }
+        Column() {
+            Text(text = "Android choice", fontSize = 16.sp)
+            Text(text = "$android", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun ButtonBox() {
+    Row(modifier = Modifier
+        .width(16.dp)
+        .wrapContentWidth(Alignment.CenterHorizontally)
+        .wrapContentHeight(Alignment.Bottom)
+        .padding(bottom = 200.dp),
+        horizontalArrangement = Arrangement.spacedBy(70.dp)
+    ) {
+        Button(text = "Rock", onClick = { /*TODO*/ }) {
+        }
+        Text(text = "paper")
+        Text(text = "Scissors")
+    }
+}
+
+@Composable
+fun JetpackText() {
+    Text(text = "#Jetpack Compose",
+        modifier = Modifier
+            .wrapContentHeight(Alignment.Bottom)
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        fontSize = 20.sp,
+        fontFamily = FontFamily.SansSerif
+        )
+}
+
+@Composable
 fun EndResult() {
     HandImg()
     Score()
+    JetpackText()
+    Choice("Rock","Scissors")
+    ButtonBox()
 }
 
 @Preview(showBackground = true, showSystemUi = true)
